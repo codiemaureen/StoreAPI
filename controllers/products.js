2025ec6d9eb0e77b65dd6b70a6a4b992631fced5
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-07 18:06:58 -0500

    seting up sort and select fields

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req,res) => {
-    const search = 'ab';
-    const products = await Product.find({
-        name:{ $regex:search, $options: 'i'},
-    });
+    const products = await Product.find({}).select('name price');
     res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts= async (req,res) => {
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
     const queryObject = {};
     if(featured){
         queryObject.featured = featured === 'true' ? true : false;
@@ -20,7 +17,20 @@ const getAllProducts= async (req,res) => {
         queryObject.name = { $regex:name, $options: 'i'}
     };
     console.log(queryObject);
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    };
+
+    if(fields){
+        const fieldsList = sort.split(',').join(' ');
+        result = result.sort(fieldsList);
+    }
+
+    const products = await result;
     res.status(200).json({products, nbHits: products.length});
 };
 
