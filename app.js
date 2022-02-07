507324ca4d9c22e147808250886c3d0064361b0c
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-07 16:04:44 -0500

    Linking server and DB

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('Store API')
+require('dotenv').config();
+//async errors
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+
+//routes
+app.get('/', (req,res) => {
+    res.send('<h1>Store Products</h1><a href="api/v1/products">Products Route</a>')
+});
+
+
+//products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on ${port}... `));
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+start();
